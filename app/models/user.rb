13e07304b68ac785b39c21e41cc55b0f777a2503
Rolando Navarrete <kinglouis.belmont@gmail.com>
2015-02-25 13:08:01 -0800

    added a test for invalid user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-
+validates_presence_of :first_name
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,16 @@ RSpec.describe User, :type => :model do
   let(:user) do
     user = User.create(first_name: "Rolando", last_name: "Navarrete", email: "wu-tang-kid@gmail.com", password_digest: "ghostface")
   end
+  
+  let(:invalid_user) do
+    invalid_user = User.create(first_name: nil, last_name: nil, email: nil, password_digest: nil)
+  end
 
   it "is valid" do
     expect(user).to be_valid
+  end
+
+  it "isn't valid" do
+    expect(invalid_user).to_not be_valid
   end
 end
