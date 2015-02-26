b02f10c9e79abbc8cc4e8c62752e6e495fa3b7b4
Rolando Navarrete <kinglouis.belmont@gmail.com>
2015-02-25 22:06:04 -0800

    deleted an unecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#home"
-  get "test", to: "welcome#test"
   resource :movies
   resource :sessions, only: [:create, :new, :destroy]
   get "/signup" => "users#new"
