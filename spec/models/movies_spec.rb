352f9faac492648baf66f9ccaf95142ce60db817
Rolando Navarrete <kinglouis.belmont@gmail.com>
2015-03-12 19:31:31 -0700

    added a column for a movie id

--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   let(:valid_movie) do
     file_path = File.open("#{Rails.root}/app/assets/images/pulp.jpg")
-    Movie.create(title: "Back to the Future", poster_image: file_path)
+    Movie.create(title: "Back to the Future", poster_image: file_path, movie_id: "back_to_the_future")
   end
 
   let(:movie_without_title) do
     file_path = File.open("#{Rails.root}/app/assets/images/pulp.jpg")
-    Movie.create(title: nil, poster_image: file_path)
+    Movie.create(title: nil, poster_image: file_path, movie_id: "casa_bonita")
+  end
+
+
+  let(:movie_without_movie_id) do
+    file_path = File.open("#{Rails.root}/app/assets/images/pulp.jpg")
+    Movie.create(title: "Back to the future", poster_image: file_path, movie_id: nil)
   end
 
   let(:movie_without_poster_image) do
@@ -23,7 +29,11 @@ RSpec.describe Movie, type: :model do
     expect(movie_without_poster_image).to_not be_valid
   end
 
+  it "isn't valid without a movie id" do
+    expect(movie_without_movie_id).to_not be_valid
+  end
+
   it "is valid when all attributes are present" do
     expect(valid_movie).to be_valid
-  end 
+  end
 end
